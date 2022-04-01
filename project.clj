26a4472483ababc34f2587e9a24bad5876c77165
Puneet Pahuja <er.puneetpahuja@gmail.com>
2022-04-01 14:43:30 +0530

    Adds logging config. Uses `logback` as logger.

--- project.clj
+++ project.clj
@@ -17,7 +17,33 @@
                  [camel-snake-kebab "0.4.2"]
                  [cheshire "5.10.2"]
                  [org.clojure/tools.logging "1.2.4"]
-                 [com.zaxxer/HikariCP "5.0.1"]]
+                 [com.zaxxer/HikariCP "5.0.1"]
+                 ;; Use Logback as the main logging implementation:
+                 [ch.qos.logback/logback-classic "1.2.11"]
+                 [ch.qos.logback/logback-core "1.2.11"]
+                 ;; Logback implements the SLF4J API:
+                 [org.slf4j/slf4j-api "1.7.36"]
+                  ;; Redirect Apache Commons Logging to Logback via the SLF4J API:
+                 [org.slf4j/jcl-over-slf4j "1.7.36"]
+                  ;; Redirect Log4j 1.x to Logback via the SLF4J API:
+                 [org.slf4j/log4j-over-slf4j "1.7.36"]
+                  ;; Redirect Log4j 2.x to Logback via the SLF4J API:
+                 [org.apache.logging.log4j/log4j-to-slf4j "2.17.2"]
+                  ;; Redirect OSGI LogService to Logback via the SLF4J API
+                 [org.slf4j/osgi-over-slf4j "1.7.36"]
+                  ;; Redirect java.util.logging to Logback via the SLF4J API.
+                  ;; Requires installing the bridge handler, see README:
+                 [org.slf4j/jul-to-slf4j "1.7.36"]]
+  :exclusions [;; Exclude transitive dependencies on all other logging
+               ;; implementations, including other SLF4J bridges.
+               commons-logging
+               log4j
+               org.apache.logging.log4j/log4j
+               org.slf4j/simple
+               org.slf4j/slf4j-jcl
+               org.slf4j/slf4j-nop
+               org.slf4j/slf4j-log4j12
+               org.slf4j/slf4j-log4j13]
   :repl-options {:init-ns investingzing.server}
   :profiles {:uberjar {:aot :all}
              :dev {:source-paths ["dev/src"]
